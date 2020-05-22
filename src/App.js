e2f4cb6ebca9cbe470766228069a2c7acfa5d61a
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-05-22 11:26:27 -0400

    Setup BuildPizza component files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import BuildPizza from './components/buildpizza/BuildPizza';
+
+import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Router>
+      <div className='App'>
+
+        <nav>
+          <Link to='/'>Home</Link>  
+        </nav>
+
+        <Link to='/pizza'>Build your pizza</Link>
+
+        <Route path='/pizza'> <BuildPizza /> </Route>
+
+      </div>
+    </Router>
   );
 };
 export default App;
---
+++ src/components/buildpizza/BuildPizza.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './buildpizza.css';
+
+const BuildPizza = props => {
+
+    return (
+
+        <h1>Build your pizza</h1>
+
+    )
+
+}
+
+
+export default BuildPizza;
